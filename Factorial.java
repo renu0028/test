cbfe923c9943d3913e16afa049d44ba12d7557e6
renu028 <renu.saini@intimetec.com>
2020-04-10 11:46:29 +0530

    Java file added

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,10 @@
 class Factorial{
 	public static void main(String args[]){
 		int i,fact=1;
-		int -Dnumber=$number;
-		for(i=1;i<=(-Dnumber=$number);i++){
+		int number=6;
+		for(i=1;i<=number;i++){
 			fact=fact*i;
 		}
-		System.out.println("Factorial of"+(-Dnumber=$number)+"is:"+fact);
+		System.out.println("Factorial of"+ number +"is:"+fact);
 	}
 }
